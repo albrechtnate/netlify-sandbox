8f2fab3c390114b26e559722a6bfdcd4b0fedf4a
Nathaniel Albrecht <nathanielalbrecht@gmail.com>
2021-07-24 14:31:03 -0400

    Add CORS Headers

--- functions/spotify_refresh_low_priviledge_access_token.js
+++ functions/spotify_refresh_low_priviledge_access_token.js
@@ -23,6 +23,10 @@ exports.handler = async (event, context) => {
 	{
 		return {
 			statusCode: 200,
+			headers: {
+				'Access-Control-Allow-Origin': 'https://nathanalbrecht.com',
+				'Vary': 'Origin',
+			},
 			body: accessToken.data.token,
 		};
 	}
